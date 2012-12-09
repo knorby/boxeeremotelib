016da3cddb15ad62e4edd6717fc9c42d854239f2
Kali Norby <kali.norby@gmail.com>
2012-12-09 00:17:36 -0800

    fixing up the package

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='boxeeremotelib',
     version='0.1',
     include_package_data=True,
-    zip_safe=False,
+    zip_safe=True,
     platforms='any',
     packages=['boxeeremotelib'],
     entry_points = {
@@ -14,4 +18,17 @@ setup(
             'boxee_command = boxeeremotelib:main'
             ],
         },
+    author = "Kali Norby",
+    author_email = "kali.norby@gmail.com",
+    description = ("an interface to the boxee remote api"),
+    license = "BSD",
+    keywords = "boxee remote entertainment library",
+    url = "https://github.com/knorby/boxeeremotelib",
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Multimedia :: Sound/Audio :: Players"
+        ],
     )
